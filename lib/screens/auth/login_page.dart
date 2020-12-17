fc9a12bee432dcc393c845de46ddc24e31aaf20d
Glauco Cetara <neocetara@hotmail.com>
2020-12-17 13:05:28 -0300

    Change Methods

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Center(
-        child: Text("Login", style: TextStyle(color: Theme.of(context).primaryColor),),
+      body: SingleChildScrollView(
+        child: _loginPageUI(),
       ),
     );
+  }
+
+  Widget _loginPageUI(){
+    return Container(
+      child: Column(
+        children: <Widget>[
+          _headingAuth(),
+          _formLogin(),
+          _loginButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _headingAuth(){
+    return Container(
+      height: 130,
+      width: 130,
+      decoration: BoxDecoration,
+    );
+  }
+
+  Widget _formLogin() {
+    return null;
+  }
+
+  Widget _loginButton() {
+    return null;
   }
 }
